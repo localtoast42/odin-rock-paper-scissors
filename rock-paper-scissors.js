e9d5f357865e3e5d0092e9b92ba02b461aefd031
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-24 15:07:59 -0500

    Implement logic for tracking wins and losses

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,12 +5,16 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (!choices.includes(playerSelection.toLowerCase())) {
-        return "Invalid player selection, try again"
+        console.log("Invalid player selection, try again")
+        playerSelection = window.prompt("Choose rock, paper, or scissors");
+        computerSelection = getComputerChoice();
+        return playRound(playerSelection, computerSelection)
     }
 
     let playerSelectionIndex = choices.indexOf(playerSelection.toLowerCase());
     let computerSelectionIndex = choices.indexOf(computerSelection.toLowerCase());
     let message = ""
+    let playerWin = 0
 
     if (playerSelectionIndex === computerSelectionIndex) {
         message = "You Tied! Try again"
@@ -18,19 +22,32 @@ function playRound(playerSelection, computerSelection) {
         message = "You Lose! " + computerSelection + " beats " + playerSelection
     } else {
         message = "You Win! " + playerSelection + " beats " + computerSelection
+        playerWin = 1
     }
     
-    return message
+    return [message, playerWin]
 }
 
 function game() {
     let playerSelection = ''
     let computerSelection = ''
+    let message = ''
+    let result = 0
+    let playerWinCount = 0
 
     for (let i = 0; i < 5; i++) {
         playerSelection = window.prompt("Choose rock, paper, or scissors");
         computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection))
+        [message, result] = playRound(playerSelection, computerSelection)
+        console.log(message)
+        playerWinCount += result
+    }
+
+    console.log("You won " + playerWinCount + " of 5 matches.")
+    if (playerWinCount > 2) {
+        console.log("You win! Play again?")
+    } else {
+        console.log("You are defeated! Play again?")
     }
 }
 
