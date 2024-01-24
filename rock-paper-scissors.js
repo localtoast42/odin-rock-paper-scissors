7ec3927c9feeacc4319a9fe4e237ea948aa84503
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-23 19:07:39 -0500

    Implement playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,29 @@
 
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    if (!choices.includes(playerSelection.toLowerCase())) {
+        return "Invalid player selection, try again"
+    }
 
-    } else
+    let playerSelectionIndex = choices.findIndex(playerSelection.toLowerCase());
+    let computerSelectionIndex = choices.findIndex(computerSelection.toLowerCase());
+    let message = ""
+
+    if (playerSelectionIndex === computerSelectionIndex) {
+        message = "You Tied! Try again"
+    } else if (((playerSelectionIndex + 1) % 3) === computerSelectionIndex) {
+        message = "You Lose! " + computerSelection + " beats " + playerSelection
+    } else {
+        message = "You Win! " + playerSelection + " beats " + computerSelection
+    }
+    
+    return message
 }
 
+const choices = ["rock", "paper", "scissors"];
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection))
