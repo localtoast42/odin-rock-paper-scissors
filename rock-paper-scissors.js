ac72ebb2710b38dad75a7093e0583deda989593d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-25 16:39:34 -0500

    Add logic to update result elements after round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,64 +1,60 @@
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)]
-}
+    return choices[Math.floor(Math.random() * choices.length)];
+};
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+
     if (!choices.includes(playerSelection.toLowerCase())) {
-        console.log("Invalid player selection, try again")
+        console.log("Invalid player selection, try again");
         playerSelection = window.prompt("Choose rock, paper, or scissors");
-        computerSelection = getComputerChoice();
-        return playRound(playerSelection, computerSelection)
-    }
+        return playRound(playerSelection);
+    };
 
     let playerSelectionIndex = choices.indexOf(playerSelection.toLowerCase());
     let computerSelectionIndex = choices.indexOf(computerSelection.toLowerCase());
-    let message = ""
-    let playerWin = 0
+    let message = "";
+    let playerWin = 0;
 
     if (playerSelectionIndex === computerSelectionIndex) {
-        console.log("You Tied! Try again")
+        console.log("You Tied! Try again");
         playerSelection = window.prompt("Choose rock, paper, or scissors");
-        computerSelection = getComputerChoice();
-        return playRound(playerSelection, computerSelection)
+        return playRound(playerSelection);
     } else if (((playerSelectionIndex + 1) % 3) === computerSelectionIndex) {
-        message = "You Lose! " + computerSelection + " beats " + playerSelection
+        message = "You Lose! " + computerSelection + " beats " + playerSelection;
     } else {
-        message = "You Win! " + playerSelection + " beats " + computerSelection
-        playerWin = 1
-    }
+        message = "You Win! " + playerSelection + " beats " + computerSelection;
+        playerWin = 1;
+    };
     
-    return [message, playerWin]
-}
-
-function game() {
-    let playerSelection = ''
-    let computerSelection = ''
-    let message = ''
-    let result = 0
-    let playerWinCount = 0
-
-    for (let i = 0; i < 5; i++) {
-        playerSelection = window.prompt("Choose rock, paper, or scissors");
-        computerSelection = getComputerChoice();
-        [message, result] = playRound(playerSelection, computerSelection)
-        console.log(message)
-        playerWinCount += result
-    }
-
-    console.log("You won " + playerWinCount + " of 5 matches.")
-    if (playerWinCount > 2) {
-        console.log("You win! Play again?")
-    } else {
-        console.log("You are defeated! Play again?")
-    }
-}
+    return [message, playerWin];
+};
 
 const choices = ["rock", "paper", "scissors"];
 const buttons = document.querySelectorAll('button');
+const roundResult = document.querySelector('#round-result');
+const runningScore = document.querySelector('#running-score');
+const gameResult = document.querySelector('#game-result');
+
+let playerWins = 0;
+let computerWins = 0;
+let gameResultMessage = '';
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        playRound(button.id);
+        if (playerWins >= 5 || computerWins >= 5) {
+            playerWins = 0;
+            computerWins = 0;
+        };
+
+        [message, result] = playRound(button.id);
+        if (result === 1) {playerWins += 1} else {computerWins += 1};
+        roundResult.textContent = message;
+        runningScore.textContent = "Player Wins: " + playerWins + " Computer Wins: " + computerWins;
+        if (playerWins >= 5 || computerWins >= 5) {
+            gameResultMessage = playerWins >= 5 ? "You win! Play again?" : "You are defeated! Play again?";
+            gameResult.textContent = gameResultMessage;
+        };
     });
 });
